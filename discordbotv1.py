5981e76127fdafdf8c9ec9b73aaee46870bb4fd1
ericlin688 <82857659+ericlin688@users.noreply.github.com>
2023-12-20 14:06:36 -0800

    Update discordbotv1.py
    Oopsy poopsy uwu

--- discordbotv1.py
+++ discordbotv1.py
@@ -12,4 +12,4 @@ client = discord.Client(intents=discord.Intents.default())
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-client.run("MTE4NDYyMjE0MjA2Nzg0NzIzOA.GmBMsw.sV8RsgwnX3dFeboO-O4HmRnJOVkQEgQ7V9uw5w")
+client.run("key")
